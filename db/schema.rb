640c203661685eb14b8b1b048501846a0e929dc5
Hani Elabed <hani.elabed@gmail.com>
2011-01-13 21:40:32 -0600

    add product and it supporting scaffold. added .rvmrc and rvm gemset export file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110114032040) do
+
+  create_table "products", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       :precision => 8, :scale => 2
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
