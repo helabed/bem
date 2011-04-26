bf6d32cb23879087a9a5296e2aaefab44ec022af
Hani Elabed <hani.elabed@gmail.com>
2011-04-26 18:39:29 -0500

    now I can test javascript in rspec and cucumber

--- features/products.feature
+++ features/products.feature
@@ -8,3 +8,16 @@ Feature: products page
 
     Then I should be on the products page
       And I should see "Products"
+      Then show me the page
+
+  @javascript
+  Scenario: want to see if I can test javascript from within cucumber
+    Given I am on the products page
+    When I follow "test js"
+    Then show me the page
+    Then I should see "js works"
+
+    Given I am on the products page
+    When I press "test button js"
+    Then show me the page
+    Then I should see "js button works"
--- spec/requests/products_page_spec.rb
+++ spec/requests/products_page_spec.rb
@@ -14,18 +14,24 @@ describe 'products page' do
     page.should have_content('Fool Mudamas')
     page.should have_content('tasty fava beans')
     page.should have_content('9.99')
-    #save_and_open_page
+    save_and_open_page
     click_on 'Edit'  # this be an Ajax button -- only works with Selenium
     fill_in 'Price', :with => '9.77'
     click_on 'Update'  # this be an Ajax button -- only works with Selenium
     page.should have_content('9.77')
-    #save_and_open_page
+    save_and_open_page
   end
 
   it "supports js", :js => true do
     visit products_path
     click_link "test js"
     page.should have_content("js works")
+    save_and_open_page
+
+    visit products_path
+    click_on "test button js"
+    page.should have_content("js button works")
+    save_and_open_page
   end
 
 end
