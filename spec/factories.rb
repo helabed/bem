8426ac70070929903b47b358c7e020d569ee11c9
Hani Elabed <hani.elabed@gmail.com>
2011-05-13 01:47:29 -0500

    work in progress in getting all tests to pass after refactoring users table

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,12 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
-  user.name                  "hani mani"
+  user.first_name            "hani"
+  user.last_name             "mani"
   user.email                 "hani@mani.com"
+  user.address               "11 rue eddison"
+  user.phone                 "011-333-4444"
+  user.city                  "Beirut"
+  user.country               "Lebanon"
   #user.name                  "Michael Hartl"
   #user.email                 "mhartl@example.com"
   user.password              "foobar"
@@ -18,7 +23,7 @@ Factory.define :micropost do |micropost|
 end
 
 Factory.define :order do |order|
-  order.name                  "hani mani"
+  order.name                  "order: 1 for hani mani"
   order.email                 "hani@mani.com"
   order.address               "101 main st"
   order.pay_type              "Cash on Delivery (COD)"
