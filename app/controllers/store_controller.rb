d3898efa0d350e36389ebcb7fb96802ccd93390b
Hani Elabed <hani.elabed@gmail.com>
2011-05-29 01:44:40 -0500

    now categories apear on side column with links that kinda work

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,8 +2,12 @@ class StoreController < ApplicationController
   layout 'store'
   def index
     @title = "Home"
-    @products = Product.all
     @cart = current_cart
+    if params[:category_id]
+      @products = Product.where(:category_id => params[:category_id])
+    else
+      @products = Product.all
+    end
   end
 
 end
