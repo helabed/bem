8a1b6bbca75fbcbccd7853d3e1e786d67310288c
Hani Elabed <hani.elabed@gmail.com>
2011-06-03 19:06:53 -0500

    now nesting categories works but needs refactoring

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -1,2 +1,10 @@
 module StoreHelper
+  def display_sub_categories(categories)
+    if categories
+      categories.map do |cat, sub_categories|
+        sub_categories ||= cat.children
+        content_tag(:div, link_to("#{cat.name}", {:controller => 'store', :action => 'index', :category_id => cat.id}) + display_sub_categories(sub_categories), :class => 'nested_categories')
+      end.join.html_safe
+    end
+  end
 end
