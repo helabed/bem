50c7c74b8f9a1841c8e5589ccc2f14faa66addd5
Hani Elabed <hani.elabed@gmail.com>
2011-06-07 23:02:55 -0500

    added image column overrides so that we can see the product image instead of its url

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,13 @@
 module ProductsHelper
+  def image_column(record)
+    unless record.image_url.blank?
+      image_tag(record.image_url(:thumb_small))
+    end
+  end
+
+  def image_show_column(record)
+    unless record.image_url.blank?
+      image_tag(record.image_url)
+    end
+  end
 end
