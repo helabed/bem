909a6eaf99a1694e216982bf807371b70254673c
Hani Elabed <hani.elabed@gmail.com>
2011-08-14 18:26:18 +0300

    corrected refresh everything script

--- script/bem/refresh_everything_from_production_and_git.sh
+++ script/bem/refresh_everything_from_production_and_git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-clear && ./script/next/refresh_all_databases_from_production.sh
-clear && ./script/next/refresh_all_nt_system_from_git.sh
+clear && ./script/bem/refresh_all_databases_from_production.sh
+clear && ./script/bem/refresh_all_bem_system_from_git.sh
