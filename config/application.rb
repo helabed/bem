a22387ec4436599c355c1f37e2917ec4bdd5b170
Hani Elabed <hani.elabed@gmail.com>
2012-08-08 13:15:50 -0500

    added corresponding file to previous CDN file

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Bem
     # config.i18n.default_locale = :de
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(jquery rails jquery-ui)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.4.2_min rails jquery-ui-1_8_13_custom_min)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
